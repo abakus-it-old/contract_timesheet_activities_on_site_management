0ef71695f473fe53881393e0e1fc4fc6a2985554
Bernard Delhez <bernarddelhez@gmail.com>
2015-08-14 09:06:09 +0200

    New on site product price

--- model/account_analytic_account.py
+++ model/account_analytic_account.py
@@ -4,3 +4,9 @@ class account_analytic_account_improvements(models.Model):
     _inherit = ['account.analytic.account']
     
     on_site_product = fields.Many2one('product.product', string="Travel Product", index=True)
+    on_site_product_price = fields.Float("Price per travel")
+
+    @api.one
+    @api.onchange('on_site_product')
+    def _set_on_site_product_price(self):
+        self.on_site_product_price = self.on_site_product.lst_price
