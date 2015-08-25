875006bb175fa2e0aa805977c3b2959c99faa06c
Bernard Delhez <bernarddelhez@gmail.com>
2015-08-25 15:05:22 +0200

    On site update

--- model/account_analytic_account.py
+++ model/account_analytic_account.py
@@ -4,9 +4,5 @@ class account_analytic_account_improvements(models.Model):
     _inherit = ['account.analytic.account']
     
     on_site_product = fields.Many2one('product.product', string="Travel Product", index=True)
-    on_site_product_price = fields.Float("Price per travel")
-
-    @api.one
-    @api.onchange('on_site_product')
-    def _set_on_site_product_price(self):
-        self.on_site_product_price = self.on_site_product.lst_price
+    on_site_invoice_by_km = fields.Boolean(string="Invoice by km", help="True: price = km * on site product public price, False: price = on site product public price")
+    on_site_distance_in_km = fields.Float("Distance in km")
