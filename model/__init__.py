41e17927115471ed5241b779fc9f608660674eae
Valentin Thirion <valentinthirion@gmail.com>
2016-05-13 11:22:36 +0200

    Ported to 9.0

--- model/__init__.py
+++ model/__init__.py
@@ -1,3 +1,2 @@
 ﻿from . import account_analytic_account
 from . import account_analytic_line
-from . import project_task_work
--- model/project_task_work.py
+++
@@ -1,6 +0,0 @@
-﻿from openerp import models, fields, api
-
-class project_task_work_improvements(models.Model):
-    _inherit = ['project.task.work']
-
-    related_on_site = fields.Boolean(related='hr_analytic_timesheet_id.line_id.on_site', store=False)
